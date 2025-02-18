66af8fc4ee8487985c5ada750526c2e340c0718f
Diaszy7 <gdias7444@gmail.com>
2025-02-18 15:10:58 -0300

    feat: add shadcn initial setup

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductPage = () => {
+  return (
+    <div className="rounded-xl border border-red-500 p-5">
+      <h1 className="text-red-500">products page</h1>
+      <Button>Clique aqui</Button>
+      <Input placeholder="Escreva"/>
+    </div>
+  );
+};
+
+export default ProductPage;
